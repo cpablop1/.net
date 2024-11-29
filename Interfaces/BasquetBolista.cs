62dfd8e728dbed77fb2e42e6309e07414cf494e2
Pablo <cpablop1@gmail.com>
2024-11-29 12:00:10 -0600

    .

---
+++ Interfaces/BasquetBolista.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    internal class BasquetBolista : Deportista, IBasquetbol
+    {
+        // Método constructor
+        public BasquetBolista(string nom, string pos, int e, int est) : base(nom, pos, e, est)
+        {
+
+        }
+
+        // Método para desarrollar la interfaz
+        public bool comprobarEstatura()
+        {
+            if (estatura >= 190)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Interfaces/Deportista.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    internal class Deportista
+    {
+
+        // Atributos
+        protected string nombres;
+        protected string posicion;
+        protected int edad;
+        protected int estatura;
+
+        // Método Constructor
+        public Deportista() { }
+        public Deportista(string nom, string pos, int e, int est)
+        {
+            nombres = nom;
+            posicion = pos;
+            edad = e;
+            estatura = est;
+        }
+
+        // Métodos de accesos (Getters/Setters)
+        /*public string NOMBRES
+        {
+            get { return nombres; }
+            set { nombres = value; }
+        }
+
+        public string POSICION
+        {
+            get { return posicion; }
+            set { posicion = value; }
+        }
+
+        public int EDAD
+        {
+            get { return edad; }
+            set { edad = value; }
+        }
+
+        public double ESTATURA
+        {
+            get { return estatura; }
+            set { estatura = value; }
+        }*/
+    }
+}
