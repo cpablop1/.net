7578f68cbe5ea0293d832cca3657b697733e8d70
Pablo <cpablop1@gmail.com>
2024-12-02 13:24:19 -0600

    Conexion con SQL Server exitosa.

---
+++ Conexion_BD/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Conexion_BD
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SqlConnection conexion = new SqlConnection("Data Source=PABLO\\SQLEXPRESS;Initial Catalog=tienda;Integrated Security=True;TrustServerCertificate=True");
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConectar_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                conexion.Open();
+
+                if (conexion.State == ConnectionState.Open)
+                {
+                    MessageBox.Show("La conexión ha sido exitosa.");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conexion.Close();
+            }
+        }
+    }
+}
